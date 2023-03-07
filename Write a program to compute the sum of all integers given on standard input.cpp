86a30a5bc86d266f3e979d2ef1a5a0893fa4186b
Steven <82331675+steven-chu@users.noreply.github.com>
2023-03-07 12:10:56 -0500

    Create Write a program to compute the sum of all integers given on standard input.cpp
    Created using C++.

---
+++ Write a program to compute the sum of all integers given on standard input.cpp
@@ -0,0 +1,13 @@
+// Write a program to compute the sum of all integers given on standard input
+#include <iostream> 
+using namespace std; 
+
+int main() {
+  int total = 0; // defining the starting point for the "sum so far" 
+  int n; // another variable to define for later 
+  while (cin >> n) { // while loop reading into standard input n 
+     total += n ; // addition where it's new total = n + previous total 
+    cout << "Sum so far: " << total << endl; 
+    }
+  return 0; 
+}
